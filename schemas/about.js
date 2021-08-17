b1dfe89115f0e7e7c853d33ccd578847c2142d83
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2021-08-17 13:32:38 +0700

    increase height input description

--- schemas/about.js
+++ schemas/about.js
@@ -1,3 +1,63 @@
+import React from 'react'
+
+import { FormField } from '@sanity/base/components'
+import { TextArea } from '@sanity/ui'
+
+import { useId } from '@reach/auto-id' // hook to generate unique IDs
+import PatchEvent, { set, unset } from '@sanity/form-builder/PatchEvent'
+
+const aboutDescription = React.forwardRef((props, ref) => {
+  const {
+    type, // Schema information
+    value, // Current field value
+    readOnly, // Boolean if field is not editable
+    placeholder, // Placeholder text from the schema
+    markers, // Markers including validation rules
+    presence, // Presence information for collaborative avatars
+    compareValue, // Value to check for "edited" functionality
+    onFocus, // Method to handle focus state
+    onBlur, // Method to handle blur state
+    onChange, // Method to handle patch events
+  } = props
+
+  // Creates a unique ID for our input
+  const inputId = useId()
+
+  // Creates a change handler for patching data
+  const handleChange = React.useCallback(
+    // useCallback will help with performance
+    (event) => {
+      const inputValue = event.currentTarget.value // get current value
+      // if the value exists, set the data, if not, unset the data
+      onChange(PatchEvent.from(inputValue ? set(inputValue) : unset()))
+    },
+    [onChange],
+  )
+
+  return (
+    <FormField
+      description={type.description} // Creates description from schema
+      title={type.title} // Creates label from schema title
+      __unstable_markers={markers} // Handles all markers including validation
+      __unstable_presence={presence} // Handles presence avatars
+      compareValue={compareValue} // Handles "edited" status
+      inputId={inputId} // Allows the label to connect to the input field
+    >
+      <TextArea
+        id={inputId} // A unique ID for this input
+        onChange={handleChange} // A function to call when the input value changes
+        value={value} // Current field value
+        readOnly={readOnly} // If "readOnly" is defined make this field read only
+        placeholder={placeholder} // If placeholder is defined, display placeholder text
+        onFocus={onFocus} // Handles focus events
+        onBlur={onBlur} // Handles blur events
+        ref={ref}
+        style={{ height: "150px" }}
+      />
+    </FormField>
+  )
+})
+
 export default {
     name: 'about',
     title: 'About',
@@ -40,6 +100,7 @@ export default {
           name: 'title',
           title: 'About Description',
           type: 'string',
+          inputComponent: aboutDescription,
           validation: Rule => Rule.required()
         },
         {
--- schemas/project_list.js
+++ schemas/project_list.js
@@ -1,3 +1,63 @@
+import React from 'react'
+
+import { FormField } from '@sanity/base/components'
+import { TextArea } from '@sanity/ui'
+
+import { useId } from '@reach/auto-id' // hook to generate unique IDs
+import PatchEvent, { set, unset } from '@sanity/form-builder/PatchEvent'
+
+const projectDescription = React.forwardRef((props, ref) => {
+  const {
+    type, // Schema information
+    value, // Current field value
+    readOnly, // Boolean if field is not editable
+    placeholder, // Placeholder text from the schema
+    markers, // Markers including validation rules
+    presence, // Presence information for collaborative avatars
+    compareValue, // Value to check for "edited" functionality
+    onFocus, // Method to handle focus state
+    onBlur, // Method to handle blur state
+    onChange, // Method to handle patch events
+  } = props
+
+  // Creates a unique ID for our input
+  const inputId = useId()
+
+  // Creates a change handler for patching data
+  const handleChange = React.useCallback(
+    // useCallback will help with performance
+    (event) => {
+      const inputValue = event.currentTarget.value // get current value
+      // if the value exists, set the data, if not, unset the data
+      onChange(PatchEvent.from(inputValue ? set(inputValue) : unset()))
+    },
+    [onChange],
+  )
+
+  return (
+    <FormField
+      description={type.description} // Creates description from schema
+      title={type.title} // Creates label from schema title
+      __unstable_markers={markers} // Handles all markers including validation
+      __unstable_presence={presence} // Handles presence avatars
+      compareValue={compareValue} // Handles "edited" status
+      inputId={inputId} // Allows the label to connect to the input field
+    >
+      <TextArea
+        id={inputId} // A unique ID for this input
+        onChange={handleChange} // A function to call when the input value changes
+        value={value} // Current field value
+        readOnly={readOnly} // If "readOnly" is defined make this field read only
+        placeholder={placeholder} // If placeholder is defined, display placeholder text
+        onFocus={onFocus} // Handles focus events
+        onBlur={onBlur} // Handles blur events
+        ref={ref}
+        style={{ height: "150px" }}
+      />
+    </FormField>
+  )
+})
+
 export default {
   name: 'project_list',
   title: 'Project List',
@@ -31,16 +91,16 @@ export default {
         }),
     },
     {
-      title: "Thumbnail",
-      name: "thumb",
-      type: "image",
-      description: "For display on Project List | JPEG / PNG / WEBP",
+      title: 'Thumbnail',
+      name: 'thumb',
+      type: 'image',
+      description: 'For display on Project List | JPEG / PNG / WEBP',
       validation: (Rule) => Rule.required(),
       fields: [
         {
-          title: "Edit Alt Text",
-          name: "name",
-          type: "string",
+          title: 'Edit Alt Text',
+          name: 'name',
+          type: 'string',
           validation: (Rule) => Rule.required(),
         },
       ],
@@ -64,15 +124,15 @@ export default {
           title: 'Keywords',
         },
         {
-          name: "seo_image",
-          title: "Image",
-          description: "800 x 600 | PNG / JPEG / WEBP | max 100kb",
-          type: "image",
+          name: 'seo_image',
+          title: 'Image',
+          description: '800 x 600 | PNG / JPEG / WEBP | max 100kb',
+          type: 'image',
           fields: [
             {
-              title: "Edit Alt Text",
-              name: "name",
-              type: "string",
+              title: 'Edit Alt Text',
+              name: 'name',
+              type: 'string',
             },
           ],
         },
@@ -81,8 +141,9 @@ export default {
     {
       name: 'title',
       title: 'Project Description',
-      type: 'blockContent',
+      type: 'string',
       description: 'For display on Project Page',
+      inputComponent: projectDescription,
       validation: (Rule) => Rule.required(),
     },
     {
