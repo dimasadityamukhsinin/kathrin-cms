fd3a61267b40b4c15e1b005c4345895f285a4986
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2022-09-22 11:46:23 +0700

    💄 update icon

--- deskStructure.js
+++ deskStructure.js
@@ -1,4 +1,7 @@
 import S from '@sanity/desk-tool/structure-builder'
+import React from 'react'
+import { FiSettings, FiFile, FiStar } from 'react-icons/fi'
+import { GrProjects } from 'react-icons/gr'
 
 export default () =>
   S.list()
@@ -6,33 +9,34 @@ export default () =>
     .items([
       ...S.documentTypeListItems().filter(
         (listItem) =>
-          ![
-            'project_list',
-            'settings',
-            'about',
-            'projects',
-            'home',
-          ].includes(listItem.getId()),
+          !['project_list', 'settings', 'about', 'projects', 'home'].includes(
+            listItem.getId(),
+          ),
       ),
       S.listItem()
         .title('Project List')
+        .icon(() => <GrProjects />)
         .child(
           S.documentTypeList('project_list').title('Project List').child(),
         ),
       S.listItem()
         .title('Pages')
+        .icon(() => <FiFile />)
         .child(
           S.list()
             .title('Pages')
             .items([
               S.listItem()
                 .title('Home')
+                .icon(() => <FiStar />)
                 .child(S.document().schemaType('home').documentId('home')),
               S.listItem()
                 .title('Info')
+                .icon(() => <FiStar />)
                 .child(S.document().schemaType('about').documentId('about')),
               S.listItem()
                 .title('Projects')
+                .icon(() => <FiStar />)
                 .child(
                   S.document().schemaType('projects').documentId('projects'),
                 ),
@@ -40,8 +44,6 @@ export default () =>
         ),
       S.listItem()
         .title('Settings')
+        .icon(() => <FiSettings />)
         .child(S.document().schemaType('settings').documentId('settings')),
-      // S.listItem()
-      //   .title('Settings')
-      //   .child(S.documentTypeList('settings').title('Settings').child()),
     ])
